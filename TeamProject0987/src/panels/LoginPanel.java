7447ca59007c98051a8a72ff80e5c3d1469dcfec
intacka <intacka@nate.comgit config --global user.email intacka@nate.comgit config --listgit config --global user.email intacka@nate.comintacka@nate.com>
2022-06-22 12:00:13 +0900

    로그인패널 로그인기능 DB연동 수정중

--- TeamProject0987/src/panels/LoginPanel.java
+++ TeamProject0987/src/panels/LoginPanel.java
@@ -2,22 +2,27 @@ package panels;
 
 
 
-import java.awt.Color;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import buttons.GoToButton;
+import database.OjdbcConnection;
 import labels.TopLabel;
 
 public class LoginPanel extends JPanel {
 	
 	//버튼,라벨,텍스트필드 생성완료
+	
 	
 	public LoginPanel() {
 		setBounds(0, 0, 1200, 800);
@@ -66,14 +71,43 @@ public class LoginPanel extends JPanel {
 		JTextField idInput = new JTextField("아이디 입력");
 		add(idInput);
 		idInput.setBounds(400, 250, 300, 30);
+		String idText = idInput.getText();
 		
 		JTextField pwInput = new JTextField("비밀번호 입력");
 		add(pwInput);
 		pwInput.setBounds(400, 300, 300, 30);
+		String pwText = pwInput.getText();
 		
 		JButton loginBtn = new JButton("로그인");
 		add(loginBtn);
 		loginBtn.setBounds(450, 350, 200, 50);
+
+		
+		try (
+				Connection conn = OjdbcConnection.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement("SELECT member_password FROM members "
+						+ "WHERE member_id = ?");
+				) {
+			pstmt.setString(1, idText);
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		loginBtn.addActionListener(new ActionListener() {	
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(idText == "") { // id,pw가 DB데이터와 일치한다면
+					MainPanel.currPanel.setVisible(true);
+					MainPanel.loginPanel.setVisible(false);
+				} else  {
+					JOptionPane.showMessageDialog(MainPanel.thisFrame, "아이디(로그인 전용 아이디) 또는 비밀번호를 잘못 입력했습니다.\r\n"
+							+ "입력하신 내용을 다시 확인해주세요.");
+				}
+			}
+		});
+		
+		
 		
 		JButton findId = new JButton("아이디찾기");
 		add(findId);
