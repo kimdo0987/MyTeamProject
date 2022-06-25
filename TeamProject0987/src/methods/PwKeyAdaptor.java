96118cf578c6d0a0debdcc5cc355264c921e8a7b
intacka <intacka@nate.comgit config --global user.email intacka@nate.comgit config --listgit config --global user.email intacka@nate.comintacka@nate.com>
2022-06-25 14:04:54 +0900

    (인택)아이디찾기, 비번찾기 - 주민등록번호 두칸으로나누기완료, 복붙제한은 없앰

--- TeamProject0987/src/methods/PwKeyAdaptor.java
+++ TeamProject0987/src/methods/PwKeyAdaptor.java
@@ -24,16 +24,16 @@ public class PwKeyAdaptor implements KeyListener  {
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(!Pattern.matches("[!-~]", ""+e.getKeyChar())
-				&&(e.getKeyChar() != KeyEvent.VK_BACK_SPACE)
-				&&(e.getKeyChar() != KeyEvent.VK_DELETE)
-				&&(e.getKeyCode() != 36) //END키 허용
-				&&(e.getKeyCode() != 35) //HOME키 허용
-				&&(e.getKeyCode() != 37) //왼쪽 방향키 허용
-				&&(e.getKeyCode() != 39) //오른쪽방향키 허용 //영문과 숫자만 허용하고 다막아놔서 따로 허용해 주어야한다
-				) {
-			e.consume(); // 이벤트 소멸(무시)하기
-		}  
+//		if(!Pattern.matches("[!-~]", ""+e.getKeyChar())
+//				&&(e.getKeyChar() != KeyEvent.VK_BACK_SPACE)
+//				&&(e.getKeyChar() != KeyEvent.VK_DELETE)
+//				&&(e.getKeyCode() != 36) //END키 허용
+//				&&(e.getKeyCode() != 35) //HOME키 허용
+//				&&(e.getKeyCode() != 37) //왼쪽 방향키 허용
+//				&&(e.getKeyCode() != 39) //오른쪽방향키 허용 //영문과 숫자만 허용하고 다막아놔서 따로 허용해 주어야한다
+//				) {
+//			e.consume(); // 이벤트 소멸(무시)하기
+//		}  
 	}
 
 	@Override
