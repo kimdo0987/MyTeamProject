3d81186ec5ec5f99a819888220ef912b9c4de8db
kevinj0695 <kevinj0695@gmail.com>
2022-06-25 21:26:03 +0900

    시범케이스 data db에 넣고, MyPaymentLog 조회문 만들고 MyPageMainPanel5 에 테이블연결 - 몽운

--- TeamProject0987/src/database/MyPaymentLog.java
+++ TeamProject0987/src/database/MyPaymentLog.java
@@ -7,7 +7,13 @@ import java.util.ArrayList;
 
 public class MyPaymentLog {
 	public static String[][] getMyPaymentLog() {
-		String sql = "";
+		String sql = "select "
+				+ "order_num, lecture_name, coupon_name, total_price, payment_date||''||'', payment_type "
+				+ "from "
+				+ "payment_log p, lecture_lists l, coupon_lists c "
+				+ "where p.lecture_id = l.lecture_id "
+				+ "AND p.coupon_id = c.coupon_id "
+				+ "AND p.member_id = 'hansm1119'";
 		
 		try (
 				Connection con = OjdbcConnection.getConnection();
@@ -17,12 +23,16 @@ public class MyPaymentLog {
 			ArrayList<String[]> list = new ArrayList<String[]>();
 			while(result.next()) {
 				list.add(new String[] {
-						result.getString("attendance_date||''||''")
-						
+						result.getString("order_num"),
+						result.getString("payment_date||''||''"),
+						result.getString("lecture_name"),
+						result.getString("coupon_name"),
+						result.getString("total_price"),
+						result.getString("payment_type")
 				});
 			}
 			System.out.println("The data(MyPaymentLog) has been fetched");
-			String[][] arr = new String[list.size()][4];
+			String[][] arr = new String[list.size()][6];
 			return list.toArray(arr);
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
