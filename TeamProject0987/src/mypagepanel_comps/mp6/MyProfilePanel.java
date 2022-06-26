abf2d53d86e28d93f17e529116306db747c02fcf
kimdo0987 <107455213+kimdo0987@users.noreply.github.com>
2022-06-27 02:31:17 +0900

    Update MyProfilePanel.java

--- TeamProject0987/src/mypagepanel_comps/mp6/MyProfilePanel.java
+++ TeamProject0987/src/mypagepanel_comps/mp6/MyProfilePanel.java
@@ -1,53 +1,31 @@
 package mypagepanel_comps.mp6;
 
-
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import database.OjdbcConnection;
 import panels.MainPanel;
 
 public class MyProfilePanel extends JPanel {
-
-	String userName;
-	String userId;
-	String userEmail;
-	String phoneNum;
-	String jNum;
-	
+	private String name;
+	private String id;
+	private String birth;
+	private String phone;
+	private String mail;
 
 	public MyProfilePanel() {
-		
-		String sql2 = "SELECT * FROM members WHERE member_id = ?";
-		try (
-				Connection conn2 = OjdbcConnection.getConnection(); 
-				PreparedStatement pstmt2 = conn2.prepareStatement(sql2);
-		) {
-			
-			pstmt2.setString(1, MainPanel.currUserId);
-			
-			try (ResultSet rs = pstmt2.executeQuery()) {
-				while (rs.next()) {
-					userId = rs.getString("member_ID");
-					userName = rs.getString("member_name");
-					userEmail = rs.getString("email");
-					jNum = rs.getString("j_number");
-					phoneNum = rs.getString("phone_number");
-					
-				}
-			}
-				
-		}catch (Exception e) {
-
-		}
-		String a = jNum.charAt(2)+""+jNum.charAt(3)+"월"+jNum.charAt(4)+""+jNum.charAt(5)+"일";
 		setLayout(null);
 
 		setBounds(0, 58, 730, 511);
@@ -105,28 +83,23 @@ public class MyProfilePanel extends JPanel {
 		});
 		add(modifyBtn);
 		
-
-		JLabel lbl1 = new JLabel(userName);
-		lbl1.setBounds(157, 106, 187, 38);
-		add(lbl1);
+		String sql1 = " SELECT * FROM members WHERE member_id = ?";
 		
-		JLabel lbl2 = new JLabel(userId);
-		lbl2.setBounds(157, 146, 187, 38);
-		add(lbl2);
+		try ( Connection conn = OjdbcConnection.getConnection();
+			  PreparedStatement pstmt = conn.prepareStatement(sql1);
+				)
+		{ 
+		pstmt.setString(1, MainPanel.currUserId);
+		ResultSet rs = pstmt.executeQuery();
 		
-		JLabel lbl3 = new JLabel(a);
-		lbl3.setBounds(157, 186, 187, 38);
-		add(lbl3);
-		
-		JLabel lbl4 = new JLabel(phoneNum);
-		lbl4.setBounds(157, 226, 187, 38);
-		add(lbl4);
-		
-		JLabel lbl5 = new JLabel(userEmail);
-		lbl5.setBounds(157, 266, 187, 38);
-		add(lbl5);
-		
-
+		while(rs.next()) { 
+			name = (rs.getString("member_name"));
+			id = (rs.getString("member_id"));
+			birth = (rs.getString("j_number"));
+			phone = (rs.getString("phone_number"));
+			mail = (rs.getString("member_id"));
+			
+		}
 		
 		lbl1.setText(name);
 		lbl2.setText(id);	
@@ -134,27 +107,27 @@ public class MyProfilePanel extends JPanel {
 		lbl4.setText(phone);	
 		lbl5.setText(mail);	
 		
-
+		} catch (SQLException e1) {
+			e1.printStackTrace();
+		}
 		
 		
 	}
-//	
-//	public static void main(String[] args) {
-//		JFrame frame = new JFrame();
-//		JPanel panel = new JPanel();
-//		frame.getContentPane().add(panel);
-//		panel.setBounds(100, 100, 1200, 800);
-//		panel.setBackground(Color.white);
-//		panel.setLayout(null);
-//
-//		panel.add(new MyProfilePanel());
-//
-//		frame.setBounds(100, 100, 1200, 800);
-//		frame.setLocationRelativeTo(null);
-//		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frame.setVisible(true);	
-//		
-//	}
+	
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel);
+		panel.setBounds(100, 100, 1200, 800);
+		panel.setBackground(Color.white);
+		panel.setLayout(null);
+
+		panel.add(new MyProfilePanel());
+
+		frame.setBounds(100, 100, 1200, 800);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);	
+		
+	}
 }
-
-
