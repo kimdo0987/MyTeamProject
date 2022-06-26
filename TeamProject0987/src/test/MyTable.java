6dd5d34545e4504499a3a91dad960bf54c270559
kevinj0695 <kevinj0695@gmail.com>
2022-06-27 00:59:05 +0900

    test패키지 MyTable 특정 행에만 밑줄, 색깔(?) 처리 완료 커밋 [몽운]

--- TeamProject0987/src/test/MyTable.java
+++ TeamProject0987/src/test/MyTable.java
@@ -1,18 +1,26 @@
 package test;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Cursor;
-import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.font.TextAttribute;
+import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
+
+import database.LectureTable.MyRenderer;
 
 public class MyTable extends JFrame {
 	
@@ -93,15 +101,53 @@ public class MyTable extends JFrame {
 			}
 		});
 		
-		table.addMouseMotionListener(new MouseAdapter() {
+		// 특정 행만 선택하기 위해 반드시 들어가야 할 코드, 이거랑 MyRenderer 메서드 설정해놓은거랑,
+		// MotionListener 에 추가한 코드 전부 들어가야하는 것을 참조 ★
+		MyRenderer cellRenderer = new MyRenderer();
+		
+//		table.getColumnModel().getColumn(0).setCellRenderer(cellRenderer);
+		table.getColumnModel().getColumn(1).setCellRenderer(cellRenderer);
+		table.getColumnModel().getColumn(2).setCellRenderer(cellRenderer);
+		table.getColumnModel().getColumn(3).setCellRenderer(cellRenderer);
+		table.getColumnModel().getColumn(4).setCellRenderer(cellRenderer);
+		table.getColumnModel().getColumn(5).setCellRenderer(cellRenderer);
+		
+		table.setEnabled(false); // 특정 행 선택 할 때 색깔 파란색 뜨는거 방지
+		// ★
+		
+		table.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseExited(MouseEvent e) {
+				setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+				table.setFont(new Font("Serif", Font.PLAIN, 13));
+			}
+		});
+		
+		
+		table.addMouseMotionListener(new MouseMotionAdapter() {
 			@Override
 			public void mouseMoved(MouseEvent e) {
 				int row = table.rowAtPoint(e.getPoint());
 		        int col = table.columnAtPoint(e.getPoint());
-		        if(col == 5) {
-		        	setCursor(new Cursor(Cursor.HAND_CURSOR));
-		        } else {
-		        	setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		        if (row >= 0 && col >= 0) {
+		        	cellRenderer.rowAtMouse = row;
+		        	cellRenderer.color = new Color(246,246,246);
+		        	table.repaint(); 
+		        	
+		        	if(col == 5) {
+		        		setCursor(new Cursor(Cursor.HAND_CURSOR));
+		        		//table.setFont(new Font("Serif", Font.BOLD, 13));
+		        		
+		        		// 여기서부터 밑줄 구현하는 법 코드입니다.
+		        		Font font = table.getFont();
+		        		Map attributes = font.getAttributes();
+		        		attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
+		        		table.setFont(font.deriveFont(attributes));
+		        	} else {
+		        		setCursor(new Cursor(Cursor.DEFAULT_CURSOR));
+		        		table.setFont(new Font("Serif", Font.PLAIN, 13));
+		        	}
+ 
 		        }
 			}
 		});
@@ -110,7 +156,30 @@ public class MyTable extends JFrame {
 		btn.setBounds(70, 30, 130, 30);
 		getContentPane().add(btn);
 		
+		table.setFont(new Font("Serif", Font.PLAIN, 13));
+		
 		setVisible(true);
+	}
+	
+	public class MyRenderer extends DefaultTableCellRenderer {
+		Color color;
+		int rowAtMouse;
+
+		@Override
+		public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
+				int row, int column) {
+			Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			if (!table.isRowSelected(row)) // 현재 선택된 행의 색상은 변경하지 않고 선택 해제된 경우에만 배경색상을 변경한다
+			{
+				if (row == rowAtMouse) {
+					c.setBackground(color);					
+				} else {
+					c.setBackground(Color.WHITE);
+					c.setFont(new Font("Serif", Font.PLAIN, 13));					
+				}
+			}
+			return c;
+		}
 	}
 	
 	// main method eventqueue 는 뭐하는 건지 모르겠는데 주석처리해도 괜찮은듯
